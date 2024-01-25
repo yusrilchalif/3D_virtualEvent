053701222b28a948a33393c0c7192e0de9a56648
Yusril <yusrilchalif@gmail.com>
2024-01-25 20:54:51 +0700

    to check movement

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity = 2.0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // Rotasi kamera berdasarkan input mouse
+        transform.Rotate(Vector3.up * mouseX * sensitivity);
+        transform.Rotate(Vector3.left * mouseY * sensitivity);
+
+        // Batasi rotasi pada sumbu Y agar kamera tidak terbalik
+        float xRotation = Mathf.Clamp(transform.rotation.eulerAngles.x, 0f, 90f);
+        transform.rotation = Quaternion.Euler(xRotation, transform.rotation.eulerAngles.y, 0f);
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10.0f;
+
+    private Rigidbody rb;
+    private Camera mainCamera;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Mengambil input dari sumbu horizontal dan vertical
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Mengambil vektor maju dari transformasi kamera
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0f; // Menetapkan nilai y menjadi 0 agar pergerakan hanya pada bidang horizontal
+
+        // Normalisasi vektor maju agar panjangnya tetap 1
+        cameraForward.Normalize();
+
+        // Menghitung vektor gerakan menggunakan input dan vektor maju kamera
+        Vector3 movement = (cameraForward * verticalInput + mainCamera.transform.right * horizontalInput).normalized;
+
+        // Mengatur kecepatan dan menerapkan gerakan ke rigidbody
+        rb.velocity = movement * speed;
     }
 }
