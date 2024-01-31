27da8f60dea58cb6a762f94141e28d8b5c1931a1
yusril <yusrilchalif@gmail.com>
2024-01-31 14:19:38 +0700

    fix video trigger

---
+++ Assets/Script/VideoPlayerCollider.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerCollider : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            print("Video play");
+            PlayVideo();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            print(" Video stop");
+            StopVideo();
+        }
+    }
+
+    private void PlayVideo()
+    {
+        if (videoPlayer != null)
+        {
+            // Putar video
+            videoPlayer.Play();
+        }
+    }
+
+    private void StopVideo()
+    {
+        if (videoPlayer != null)
+        {
+            // Hentikan video
+            videoPlayer.Stop();
+        }
+    }
+}
