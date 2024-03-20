58dbf89e4d5d2165642ceceeba411f32b362f1ae
yusril <yusrilchalif@gmail.com>
2024-03-20 10:44:15 +0700

    fix parsing

---
+++ Assets/Script/Parse/AutoPlayVideo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class AutoPlayVideo : MonoBehaviour
+{
+    [SerializeField] string videoName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayVideo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoName);
+
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+
+}
