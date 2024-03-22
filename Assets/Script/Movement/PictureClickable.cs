2507efb0a12ca5171f430d63525926845c407c77
yusril <yusrilchalif@gmail.com>
2024-03-22 14:40:24 +0700

    to testing move to picture

---
+++ Assets/Script/Movement/PictureClickable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureClickable : MonoBehaviour
+{
+    public CharacterController controller; // Reference to your CharacterController component
+    public float moveSpeed = 5f; // Kecepatan pergerakan player
+    public float rotateSpeed = 5f; // Kecepatan rotasi player
+    public float distanceToStop = 2f; // Jarak berhenti player dari lukisan
+
+    private Transform paintingTransform; // Transform lukisan yang dipilih
+
+    void Update()
+    {
+        // Menjalankan fungsi RaycastToPainting hanya ketika player mengklik kiri mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Melakukan raycast dari kamera utama
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                // Jika raycast mengenai objek dengan tag "Picture"
+                if (hit.collider.gameObject.tag == "Picture")
+                {
+                    // Menyimpan transform lukisan yang dipilih
+                    paintingTransform = hit.collider.gameObject.transform;
+
+                    // Menghitung arah dan jarak ke lukisan
+                    Vector3 direction = (paintingTransform.position - transform.position).normalized;
+                    float distanceToPainting = Vector3.Distance(transform.position, paintingTransform.position);
+
+                    // Menghitung waktu yang dibutuhkan untuk mencapai lukisan
+                    float timeToReachPainting = distanceToPainting / moveSpeed;
+
+                    // Memindahkan player langsung ke lukisan
+                    controller.Move(direction * distanceToPainting);
+
+                    // Merotasi player agar menghadap lukisan
+                    Quaternion targetRotation = Quaternion.LookRotation(paintingTransform.position - transform.position);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+                    // Menghentikan pergerakan player ketika mencapai jarak tertentu
+                    if (distanceToPainting <= distanceToStop)
+                    {
+                        // Menghentikan pergerakan player
+                        moveSpeed = 0f;
+
+                        // Menghentikan rotasi player
+                        rotateSpeed = 0f;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/TouchToMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TouchToMove : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent player;
+    public GameObject targetDestination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                targetDestination.transform.position = hit.point;
+                player.SetDestination(hit.point);
+            }
+        }
+    }
+}
